dc86ac02c489621368dc8de3ef23de6fb39af025
VAnessa Gonzalez <vanetsa846@gmail.com>
2026-02-03 13:17:13 -0800

    tried adding a login_required feature but was too much work.

--- jokes/views.py
+++ jokes/views.py
@@ -3,6 +3,7 @@ import json
 from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
@@ -35,7 +36,7 @@ class JokeDeleteView(UserPassesTestMixin, DeleteView):
     def test_func(self):
         obj = self.get_object()
         return self.request.user == obj.user
-    
+
 
     def form_valid(self, form):
         messages.success(self.request, 'Joke deleted.')
@@ -67,7 +68,7 @@ def vote(request, slug):
     data = json.loads(request.body) # Data from the JavaScript.
 
     # Set simple variables.
-    vote = data['vote'] # The user's new vote.
+    user_vote = data['vote'] # The user's new vote.
     likes = data['likes'] # The number of likes currently displayed on page.
     dislikes = data['dislikes'] # The number of dislikes currently displayed.
 
@@ -78,14 +79,14 @@ def vote(request, slug):
             # User already voted. Get user's past vote:
             joke_vote = JokeVote.objects.get(user=user, joke=joke)
 
-            if joke_vote.vote == vote: # User's new vote is the same as old vote.
+            if joke_vote.vote == user_vote: # User's new vote is the same as old vote.
                 msg = 'Right. You told us already. Geez.'
             else: # User changed vote.
-                joke_vote.vote = vote # Update JokeVote instance.
+                joke_vote.vote = user_vote # Update JokeVote instance.
                 joke_vote.save() # Save.
 
                 # Set data to return to the browser.
-                if vote == -1:
+                if user_vote == -1:
                     likes -= 1
                     dislikes += 1
                     msg = "Don't like it after all, huh? OK. Noted."
@@ -95,11 +96,11 @@ def vote(request, slug):
                     msg = 'Grown on you, has it? OK. Noted.'
         else: # First time user is voting on this joke.
             # Create and save new vote.
-            joke_vote = JokeVote(user=user, joke=joke, vote=vote)
+            joke_vote = JokeVote(user=user, joke=joke, vote=user_vote)
             joke_vote.save()
 
             # Set data to return to the browser.
-            if vote == -1:
+            if user_vote == -1:
                 dislikes += 1
                 msg = "Sorry you didn't like the joke."
             else:
