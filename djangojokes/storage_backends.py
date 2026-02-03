39533f331eb694e196ff9411d86cd8d9d609723a
VAnessa Gonzalez <vanetsa846@gmail.com>
2026-02-02 22:56:59 -0800

    Initial commit - credentials removed

---
+++ djangojokes/storage_backends.py
@@ -0,0 +1,24 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class StaticStorage(S3Boto3Storage):
+    """ Class for storing static files. """
+    location = 'static'
+    default_acl = None
+    file_overwrite = True
+
+class PublicMediaStorage(S3Boto3Storage):
+    """ Class for storing public media files. """
+    location = 'media/public'
+    default_acl = 'public-read'
+    file_overwrite = False
+
+    # This prevents Django from checking S3 before uploading
+    def exists(self, name):
+        return False
+
+class PrivateMediaStorage(S3Boto3Storage):
+    """ Class for storing private media files. """
+    location = 'media/private'
+    default_acl = 'private'
+    file_overwrite = False
+    custom_domain = False
---
+++ jokes/models.py
@@ -0,0 +1,94 @@
+from django.db import models
+from django.urls import reverse
+from django.conf import settings
+
+from common.utils.text import unique_slug
+
+
+# Create your models here.
+class Joke(models.Model):
+    question = models.TextField(max_length=200)
+    answer = models.TextField(max_length=100, blank=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT
+    )
+    category = models.ForeignKey(
+        'Category', on_delete=models.PROTECT
+    )
+    tags = models.ManyToManyField('Tag')
+    slug = models.SlugField(
+        max_length=50, unique=True, null=False, editable=False
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    def get_absolute_url(self):
+        return reverse('jokes:detail', args=[self.slug])
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+
+        #saves object, if w/o then object not saved
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.question
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=50)
+    slug = models.SlugField(
+        max_length=50, unique=True, null=False, editable=False
+    )
+    created  = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    def get_absolute_url(self):
+        return reverse('jokes:category', args={self.slug})
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.category
+
+
+    class Meta:
+        ordering = ['category']
+        verbose_name_plural = 'Categories'
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=50)
+    slug = models.SlugField(
+        max_length=50, unique=True, null=False, editable=False
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('jokes:tag', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.tag
+
+
+    class Meta:
+        ordering = ['tag']
