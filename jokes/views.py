6d7ada845c6e58d16be48221109e211b33d43b19
VAnessa Gonzalez <vanetsa846@gmail.com>
2026-02-04 13:02:04 -0800

    implementad a serch bar and button works w/ sorting aswell

--- jokes/views.py
+++ jokes/views.py
@@ -3,10 +3,11 @@ import json
 from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+
+from django.db.models import Q
 
 from .models import Joke, JokeVote
 from .forms import JokeForm
@@ -110,6 +111,14 @@ class JokeListView(ListView):
     def get_queryset(self):
         ordering = self.get_ordering()
         qs = Joke.objects.all()
+
+
+        if 'q' in self.request.GET: # Filter by search query
+            q = self.request.GET.get('q') 
+            qs = qs.filter(
+                Q(question__icontains=q) | Q(answer__icontains=q)
+            )
+
 
         if 'slug' in self.kwargs: # Filter by category or tag
             slug = self.kwargs['slug']
