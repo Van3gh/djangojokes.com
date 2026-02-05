855b59c98f987b2657854297bfa5dd567104973e
VAnessa Gonzalez <vanetsa846@gmail.com>
2026-02-05 15:08:40 -0800

    unregistered social apps

--- users/admin.py
+++ users/admin.py
@@ -5,6 +5,7 @@ from common.admin import DjangoJokesAdmin
 from common.utils.admin import append_fields, move_fields, remove_fields
 from django.utils.safestring import mark_safe
 from django.urls import reverse
+from allauth.socialaccount.models import SocialApp, SocialAccount, SocialToken
 
 # Register your models here.
 CustomUser = get_user_model()
@@ -47,3 +48,9 @@ class CustomUserAdmin(DjangoJokesAdmin, UserAdmin):
     def get_form(self, request, obj=None, **kwargs):
         self.save_on_top = obj is not None
         return super().get_form(request, obj, **kwargs)
+    
+
+    # unregisters/removes social apps
+    admin.site.unregister(SocialApp)
+    admin.site.unregister(SocialAccount)
+    admin.site.unregister(SocialToken)
